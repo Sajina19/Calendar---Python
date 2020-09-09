889ec6e70564ebfda5bd395b2a97f88b719e795e
Sajina19 <65849784+Sajina19@users.noreply.github.com>
2020-09-09 19:08:43 +0530

    Add files via upload

---
+++ calendar.py
@@ -0,0 +1,11 @@
+#impoting tkinter
+from tkinter import *
+#importing calendar module
+import calendar
+root=Tk()
+root.title("Calendar")
+year = 2020
+myCal=calendar.calendar(year)
+cal_year = Label(root,text=myCal,font = "Consolas 10 bold")
+cal_year.pack()
+root.mainloop()
